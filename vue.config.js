7533ac8da050e9b95474f6d781321ed6046ddda5
Dmitriy Buzinin <dbuzinin@voximplant.com>
2023-12-14 13:05:07 +0300

    initial commit

---
+++ vue.config.js
@@ -0,0 +1,49 @@
+const {ModuleFederationPlugin} = require('webpack').container;
+
+const deps = require('./package.json').dependencies;
+module.exports = {
+  publicPath: "auto",
+  configureWebpack: () => {
+    return {
+      performance: {
+        hints: false,
+      },
+      optimization: {
+         // Имена модулей будут осмысленными. Помогает при отладке. На размер бандла влияет крайне мало
+         moduleIds: 'named',
+         // Имена чанков будут осмысленными. Помогает при отладке. На размер бандла влияет крайне мало
+         chunkIds: 'named',
+        splitChunks: false,
+      },
+      devtool: process.env.VUE_APP_SENTRY_RELEASE ? 'source-map' : 'eval-cheap-module-source-map',
+      devServer: {
+        https: true,
+        client: {
+          overlay: false,
+        },
+      },
+      plugins: [
+        new ModuleFederationPlugin({
+          name: "remote_app",
+          filename: 'remoteEntry.js',
+          exposes: {
+            './HelloWorld': './src/components/HelloWorld',
+          },
+          library: { type: 'var', name: 'remote_app' },
+          /*shared: require('./package.json').dependencies,*/
+          shared: {
+            vue: {
+              requiredVersion: deps.vue,
+              singleton: true,
+              eager: true
+            },
+          }
+        }),
+        // FEATURE этот плагин делает все в prefetch в index.html, надо поисследовать, так как после этого стала страничка грузиться чуть медленнее
+      ].filter(Boolean),
+    };
+  },
+
+
+  lintOnSave: process.env.NODE_ENV !== 'production' ? 'warning' : 'error',
+};
